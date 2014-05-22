69b7ec8d1e40a047b3576ae52924963a4f20a5fb
Brian Dailey <github@dailytechnology.net>
2014-05-22 10:39:20 -0500

    Check METADATA, too.

--- check.py
+++ check.py
@@ -10,21 +10,20 @@ def main():
     parser = argparse.ArgumentParser(description="Read all installed packages from sys.path and list licenses.")
     args = parser.parse_args()
 
+    meta_files_to_check = ['PKG-INFO', 'METADATA']
+
     for installed_distribution in get_installed_distributions():
         found_license = False
-        try:
-            for line in installed_distribution.get_metadata_lines('PKG-INFO'):
+        for metafile in meta_files_to_check:
+            if not installed_distribution.has_metadata(metafile):
+                continue
+            for line in installed_distribution.get_metadata_lines(metafile):
                 if 'License: ' in line:
                     (k, v) = line.split(': ', 1)
                     sys.stdout.write("{project_name}: {license}\n".format(
                         project_name=installed_distribution.project_name,
                         license=v))
                     found_license = True
-        except IOError as e:
-            # usually because the PKG-INFO file was not found.
-            sys.stderr.write("Failed to get license information for {project_name}\n".format(
-                project_name=installed_distribution.project_name))
-            sys.stderr.write(str(e) + "\n")
         if not found_license:
             sys.stdout.write("{project_name}: Found no license information.\n".format(
                 project_name=installed_distribution.project_name))
